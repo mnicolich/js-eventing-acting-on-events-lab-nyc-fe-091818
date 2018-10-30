8bd837fbcd15ac7c30c305c5c78a535607022ff6
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-30 23:54:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = 'pink'
 
 let dodgerPosition = 0 
-dodger.style.left = '330px'
-function moveDodger() {
+// dodger.style.left = '330px'
+
+function moveDodger(event) {
   
 }
 dodger.addEventlistener('keydown', moveDodger)
