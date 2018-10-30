9bbe7aa50f0dd89d40403b78175913877363bae6
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-30 23:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let dodgerPosition = 0
 // dodger.style.left = '330px'
 
 function moveDodger(event) {
-  console.log(event.which)
+ const keycode = (event.which)
+ 
 }
 dodger.addEventlistener('keydown', moveDodger)
