0d43dad205ae06be9a14b21d7c0103c0bec26f36
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-31 00:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ let dodgerPosition = 0
 function moveDodger(event) {
  const keycode = (event.which)
  if (keycode === 37) {
-   
+   // this is the left arrow key 
+ } else if (keycode === 39) {
+   // this is the right arrow key
+ } else if (keycode === 38) {
+   // up arrow key
+ } else if (keycode === 40) {
+   //down arrow key 
  }
 }
 dodger.addEventlistener('keydown', moveDodger)
